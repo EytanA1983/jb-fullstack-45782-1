08d11d7941b4f5d9de5051708ad62ce86837dbbb
Eytan1983 <ethanrdt@gmail.com>
2025-09-07 13:45:12 +0300

    tov

---
+++ 2025-07-01-javascript/15-print-styled-name/app.js
@@ -0,0 +1,17 @@
+const firstName = prompt("enter first name");
+const lastName = prompt("enter last name");
+
+styleName(firstName);
+styleName("eytan");
+styleName(lastName);
+
+function styleName(argument) {
+  console.log(`${argument[0].toUpperCase()}${argument.substring(1)}`);
+}
+
+console.log("hello");
+console.log("world");
+
+
+firstName.indexOf('h')
+ 
